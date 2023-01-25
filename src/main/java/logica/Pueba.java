cbb406617b57e8569916207c2d97bfd7c94b4dcd
Cristian Diaz Vega <alexander85vega@gmail.com>
2023-01-25 20:19:22 -0300

    Copiador de texto con interfaz

---
+++ src/main/java/logica/Pueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author cristian
+ */
+public class Pueba {
+
+    public static void main(String[] args) {
+        Pantalla pant =new Pantalla();
+        pant.setVisible(true);
+        pant.setLocationRelativeTo(null);
+    }
+}
